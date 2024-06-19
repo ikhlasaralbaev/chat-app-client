272b46aa0ba0765cf094a35f2841419583398bf8
Ikhlasdev17 <iklasaralbaev@gmail.com>
2024-06-19 15:46:53 +0500

    fix: readme md file changed

--- src/api/axios.interceptor.ts
+++ src/api/axios.interceptor.ts
@@ -1,15 +1,12 @@
 import axios from 'axios'
 export const baseUrl = 'http://127.0.0.1:8080'
-// Create an instance of axios
 const axiosInstance = axios.create({
-	baseURL: 'http://127.0.0.1:8080/api', // Replace with your API base URL
-	timeout: 10000, // optional, set a timeout for requests
+	baseURL: 'http://127.0.0.1:8080/api',
+	timeout: 10000,
 })
 
-// Request Interceptor
 axiosInstance.interceptors.request.use(
 	config => {
-		// Add authorization header if token exists
 		const token = localStorage.getItem('chat-token')
 		if (token) {
 			config.headers['Authorization'] = `Bearer ${token}`
@@ -17,7 +14,6 @@ axiosInstance.interceptors.request.use(
 		return config
 	},
 	error => {
-		// Handle request errors
 		return Promise.reject(error)
 	}
 )
